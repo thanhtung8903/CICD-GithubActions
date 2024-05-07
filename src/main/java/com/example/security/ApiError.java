cb5f62ea17162e6fd136e3797a9c1cf6dccc9fdf
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-07 23:56:42 +0700

    first commit

---
+++ src/main/java/com/example/security/ApiError.java
@@ -0,0 +1,11 @@
+package com.example.security;
+
+import java.time.LocalDateTime;
+
+public record ApiError(
+        String path,
+        String message,
+        int status,
+        LocalDateTime timestamp
+) {
+}
---
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    // User Creation
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        // InMemoryUserDetailsManager
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    // Configuring HttpSecurity
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/auth/welcome").permitAll()
+                                .requestMatchers("/auth/user/**").authenticated()
+                                .requestMatchers("/auth/admin/**").authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    // Password Encoding
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
